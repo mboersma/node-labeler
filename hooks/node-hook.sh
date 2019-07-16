43e190cd96cd62272a1c70f5b15e0041e6b0a701
Matt Boersma <Matt.Boersma@microsoft.com>
2019-07-16 13:10:23 -0600

    feat: k8s operator that applies master and agent node labels

---
+++ hooks/node-hook.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+if [[ $1 == "--config" ]]; then
+  cat <<EOF
+{
+  "onKubernetesEvent": [
+    {
+      "name": "OnNodeAdded",
+      "kind": "node",
+      "event": ["add"]
+    },
+    {
+      "name": "OnNodeUpdated",
+      "kind": "node",
+      "event": ["update"],
+      "jqFilter": ".metadata.labels"
+    }
+  ]
+}
+EOF
+else
+  set -euo pipefail
+
+  MASTER_LABELS="kubernetes.azure.com/role=master kubernetes.io/role=master node-role.kubernetes.io/master="
+  AGENT_LABELS="kubernetes.azure.com/role=agent kubernetes.io/role=agent node-role.kubernetes.io/agent="
+  resourceName=$(jq -r '.[0].resourceName' "$BINDING_CONTEXT_PATH")
+  currentLabels=$(kubectl label node "$resourceName" --list)
+
+  # check the existing node labels to identify it as a master or agent
+  labelsToApply=$AGENT_LABELS
+  for label in $currentLabels; do
+    if [[ $MASTER_LABELS == *$label* ]]; then
+      labelsToApply=$MASTER_LABELS
+      break
+    fi
+  done
+
+  # check the existing node labels again to see if any are missing
+  COUNTER=0
+  for label in $currentLabels; do
+    if [[ $labelsToApply == *$label* ]]; then
+      COUNTER=$((COUNTER + 1))
+    fi
+  done
+
+  if [[ $COUNTER -lt 3 ]]; then
+    # apply all expected master or agent labels to the node
+    # TODO: log this prefixed by "2019-07-16T18:48:16Z INFO     : "
+    kubectl label --overwrite node "$resourceName" $labelsToApply
+  fi
+fi
